f6189ae0fde92d704a10a9687717a2db4fc67328
Daniela Escobar <daniela.b.escobar@gmail.com>
2023-03-18 20:25:45 +0100

    remove unnecessary lines in quickstart.sh

--- quickstart.sh
+++ quickstart.sh
@@ -9,14 +9,4 @@ DOTNET_PID=$!
 
 # Start the React frontend
 cd ../client
-npm start &
-
-# Save the PID of the npm process
-NPM_PID=$!
-
-# Wait for user input to stop the servers
-read -p "Press any key to stop the servers..."
-
-# Kill the dotnet and npm processes
-kill $DOTNET_PID
-kill $NPM_PID
+npm start 
