d7a73b915bcf1a5ea21cdd5d5f84b9a2df54b286
Daniela Escobar <daniela.b.escobar@gmail.com>
2023-03-19 10:48:01 +0100

    init commit

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -6,6 +6,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", builder =>
+    {
+        builder.WithOrigins("http://localhost:3000")
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
